ac1696eca2552caa35c6871631d627842696b108
Terrence Wang <terrencewang122@gmail.com>
2018-07-13 10:53:43 +1000

    modify wrok collection adding one more

--- src/containers/Works/Work/Work.js
+++ src/containers/Works/Work/Work.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import Landing from '../../../assets/sample-image-1.png';
+import React, {Component} from 'react';
 import classes from './Work.css';
+import Landing from '../../../assets/sample-image-1.png';
+import Modern from '../../../assets/sample-image-2.png';
 
-const Work = () => (
-  <div className={classes.Work}>
-    <h2>Responsive Landing Page</h2>
-    <p>This is a fully responsive website suiteable for small businesses or individuals to use as a landing page</p>
-    <a href="https://terrencewang302.github.io/landingPage/" target="_blank" rel="noopener noreferrer">
-      <img src={Landing} alt="Page interface" />
-    </a>
-  </div>
-);
+class Work extends Component {
+  state = {
+    works : [
+      {
+        title: 'Responsive Landing Page',
+        description: 'This is a fully responsive website suiteable for small businesses or individuals to use as a landing page',
+        link: 'https://terrencewang302.github.io/landingPage/',
+        imageSource: Landing
+      },
+      {
+        title: 'Modern Responsive Home Page',
+        description: 'A template built with Bootstrap and jQuery',
+        link: 'https://terrencewang302.github.io/modern/',
+        imageSource: Modern
+      }
+    ]
+  };
+
+  render() {
+    const works = this.state.works.map(wk => {
+      return (
+        <div className={classes.Indi}>
+          <h2>{wk.title}</h2>
+          <p>{wk.description}</p>
+          <a href={wk.link} target="_blank" rel="noopener noreferrer">
+            <img src={wk.imageSource} alt="Page interface" />
+          </a>
+        </div>
+      );
+    });
+
+    return (
+      <div className={classes.Work}>
+        {works}
+      </div>
+    );
+  }
+}
 
 export default Work;
--- src/containers/Works/Works.js
+++ src/containers/Works/Works.js
@@ -4,11 +4,11 @@ import Thin from '../../components/Bars/Thin';
 import Work from './Work/Work';
 
 const Works = () => (
-  <div className={classes.Works}>
-    <h1>Keep Perfecting <strong>MY WORKS</strong></h1>
-    <Thin />
-    <Work />
-  </div>
+      <div className={classes.Works}>
+        <h1>Keep Perfecting <strong>MY WORKS</strong></h1>
+        <Thin />
+        <Work />
+      </div>
 );
 
 export default Works;
